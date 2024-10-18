d90821bab44f685f1000aca57ce349a288d87e9a
hiy0ung <hyejee98@gmail.com>
2024-10-18 17:43:53 +0900

    20241018

---
+++ src/main/java/org/example/springbootdeveloper/dto/request/CommentRequestDto.java
@@ -0,0 +1,11 @@
+package org.example.springbootdeveloper.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CommentRequestDto {
+    private Long postId; // 댓글이 달릴 게시글 ID
+    private String content;
+    private String commenter;
+}
+
