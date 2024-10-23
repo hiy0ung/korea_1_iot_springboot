45c25d5e84e36b658d3f3bb803243b33397441bc
hiy0ung <hyejee98@gmail.com>
2024-10-23 14:29:47 +0900

    20241023

--- src/main/java/org/example/springbootdeveloper/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/example/springbootdeveloper/filter/JwtAuthenticationFilter.java
@@ -19,19 +19,29 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 /*
-    JWT 인증 필터
-    - 요청에서 JWT 토큰을 추출
-    - 해당 토큰을 검증하여 유효하다면 SecurityContext에 사용자 정보를 등록
-    - OncePerRequestFilter
-    : 모든 요청마다 한 번씩 필터가 실행되도록 보장
-*/
+* JWT 인증 필터
+* : 요청에서 JWT 토큰을 추출
+* : request의 header에서 토큰을 추출하여 검증
+* : security의 context에 접근 제어자 정보 등록
+*
+* - OncePerRequestFilter
+* : 모든 요청마다 한 번씩 필터가 실행되도록 보장
+* */
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     // JWT 토큰을 처리하는 JwtProvider 의존성 주입
+    // : JWT 검증에 사용
     private final JwtProvider jwtProvider;
 
+    /*
+    * doFilterInternal
+    * : 요청의 헤더에서 JWT 토큰을 추출
+    * : JwtProvider에서 만든 removeBearer()을 호출하여 토큰을 파싱
+    * : JwtProvider를 사용하여 토큰 검증 및 사용자 ID 추출
+    * : 추출한 사용자 ID를 바탕으로 SecurityContext에 인증 정보를 설정하는 메서드 호출
+    * */
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
@@ -39,17 +49,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
         try {
-            // 요청의 Authorization 헤더에서 Bearer 토큰 추출
-            String token = parseBearerToken(request);
+            // 요청 헤더에서 JWT 토큰 추출
+            String authorizationHeader = request.getHeader("Authorization");
+
+            // 헤더에서 토큰을 파싱하여 가져옴 ("Bearer "을 제거)
+            String token = (authorizationHeader != null && authorizationHeader.startsWith("Bearer "))
+                    ? jwtProvider.removeBearer(authorizationHeader)
+                    : null;
 
             // 토큰이 없거나 유효하지 않으면 필터 체인을 타고 다음 단계로 이동
-            if (token == null) {
+            if (token == null || !jwtProvider.isValidToken(token)) {
                 filterChain.doFilter(request, response);
                 return;
             }
 
             // JWT 토큰이 유효할 경우 해당 토큰에서 사용자 ID 추출
-            String userId = jwtProvider.validate(token);
+            String userId = jwtProvider.getUserIdFromJwt(token);
 
             // 추출한 사용자 ID를 바탕으로 securityContext에 인증 정보 설정
             setAuthenticationContext(request, userId);
@@ -58,12 +73,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             e.printStackTrace();
         }
 
+        filterChain.doFilter(request, response);
     }
 
     /*
-        setAuthenticationContext
-        : SecurityContext에 인증 정보를 설정하는 메서드
-    */
+    * setAuthenticationContext
+    * : SecurityContext에 인증 정보를 설정하는 메서드
+    * */
     private void setAuthenticationContext(HttpServletRequest request, String userId) {
         // 사용자 ID를 바탕으로 UsernamePasswordAuthenticationToken 생성
         // : 권한 정보 없음
@@ -79,23 +95,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         // SecurityContextHolder에 생성된 컨텍스트를 설정
         SecurityContextHolder.setContext(securityContext);
-    }
-
-    /*
-        요청의 Authorization 헤더에서 Bearer 토큰 추출하는 메서드
-
-        @param: request(HttpServletRequest) 요청 객체
-        @return: JWT 토큰 문자열, 없으면 null 반환
-    */
-    private String parseBearerToken(HttpServletRequest request) {
-        // Authorization 헤더에 값을 추출
-        String authorizationHeader = request.getHeader("Authorization");
-
-        // 헤더가 비어있거나 형식이 잘못되었는지 확인
-        if (!StringUtils.hasText(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
-            return null;
-        }
-        // "Bearer " 이후의 실제 JWT 토큰 값만 반환
-        return authorizationHeader.substring(7);
     }
 }
